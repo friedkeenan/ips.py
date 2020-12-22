5cc9aef65bea450923bb8b6cbf592b8d1a93eddf
friedkeenan <friedkeenan@protonmail.com>
2020-12-21 22:52:08 -0600

    Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="ips.py",
-    version="0.1.1",
-    author="friedkeenan",
-    description="A Python library for handling IPS patches",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/friedkeenan/ips.py",
-    packages=setuptools.find_packages(),
-    classifiers=[
+    name                          = "ips.py",
+    version                       = "0.1.2",
+    author                        = "friedkeenan",
+    description                   = "A Python library for handling IPS patches",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    url                           = "https://github.com/friedkeenan/ips.py",
+    packages                      = setuptools.find_packages(),
+    classifiers                   = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires               = ">=3.6",
 )
